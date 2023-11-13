bd902819847b8f3fa2ac874afa6d1456196a0a30
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2023-11-13 15:39:46 -0600

    Fixed event listeners

--- prop.js
+++ prop.js
@@ -1,9 +1,30 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+function setupEventListeners() {
+    // Event Delegation for Dropdowns
+    document.addEventListener('click', (event) => {
+        // Check if the clicked element is an h3 within a skill-category
+        if (event.target.matches('.skill-category h3') || event.target.closest('.skill-category h3')) {
+            const skillCategory = event.target.closest('.skill-category');
+            if (skillCategory) {
+                const content = skillCategory.querySelector('.skill-content');
+                if (content) {
+                    content.classList.toggle('hidden');
+                }
+            }
+        }
     });
-});
+
+    // Smooth Scrolling for anchor tags
+    document.addEventListener('click', (event) => {
+        const anchor = event.target.closest('a[href^="#"]');
+        if (anchor) {
+            event.preventDefault();
+            const target = document.querySelector(anchor.getAttribute('href'));
+            if (target) {
+                target.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    });
+}
+
+// Immediate invocation
+setupEventListeners();
