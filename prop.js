f993a273653e94fae216fc337ec82c7689a1808b
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2024-02-29 10:36:00 -0600

    Redesigned projects on website

--- prop.js
+++ prop.js
@@ -5,18 +5,25 @@ function setupEventListeners() {
     document.addEventListener('click', (event) => {
         const anchor = event.target.closest('a[href^="#"]');
         if (anchor) {
-            event.preventDefault();
-            const target = document.querySelector(anchor.getAttribute('href'));
-            if (target) {
-                target.scrollIntoView({ behavior: 'smooth' });
+            const href = anchor.getAttribute('href');
+            // Check if the href is not just "#"
+            if (href !== "#") {
+                event.preventDefault();
+                const target = document.querySelector(href);
+                if (target) {
+                    target.scrollIntoView({ behavior: 'smooth' });
+                }
             }
         }
     });
+
 
     window.addEventListener('scroll', function() {
         var offset = window.pageYOffset;
         document.body.style.backgroundPositionY = offset * 0.7 + "px"; // The multiplier can be adjusted for speed
     });
+
+
     
 
 }
@@ -75,6 +82,27 @@ function toggleProjectDetails(event, detailsId) {
     details.classList.toggle('hidden');
 }
 
+// Dropdown interaction
+document.querySelectorAll('.dropdown-btn').forEach(button => {
+    button.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent navigating to "#"
+        var dropdownContent = this.nextElementSibling;
+        dropdownContent.classList.toggle('show');
+    });
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
 
 // // Wait for the DOM to load before setting up event listeners
 // document.addEventListener('DOMContentLoaded', () => {
