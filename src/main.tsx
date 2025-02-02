cff1451d866491ceef87444780106af529622b55
Niladri Chatterjee <code.niladri@gmail.com>
2025-02-03 01:24:58 +0530

    Fix refresh problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,25 @@
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import App from './App.tsx';
+// import './index.css';
+
+// const root = document.getElementById('root');
+// if (!root) throw new Error('Root element not found');
+
+// createRoot(root).render(
+// 	<StrictMode>
+// 		<BrowserRouter>
+// 			<App />
+// 		</BrowserRouter>
+// 	</StrictMode>
+// );
+
+
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // Change from BrowserRouter
 import App from './App.tsx';
 import './index.css';
 
@@ -9,8 +28,8 @@ if (!root) throw new Error('Root element not found');
 
 createRoot(root).render(
 	<StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</StrictMode>
 );
