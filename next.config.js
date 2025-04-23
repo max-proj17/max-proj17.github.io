386a3476da5d214e209ca657892a199f9d89bba3
Niladri Chatterjee <code.niladri@gmail.com>
2025-04-23 08:42:18 +0530

    offline file concept

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+	fallbacks: {
+		document: '/offline', // path to your offline fallback page
+	},
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -13,4 +22,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
